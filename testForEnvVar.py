cae0213248e67fc74e88769d3c0d53656487fc8d
Alexander Mitupov <alex@Alexs-MBP.local>
2020-10-20 01:05:51 +0700

    add info about environmental variables to howTo10.txt

--- testForEnvVar.py
+++ testForEnvVar.py
@@ -1,7 +1,10 @@
 import os 
 
 db_user = os.environ.get('DB_USER')
-db_user = os.environ.get('DB_PASS')
+db_pass = os.environ.get('DB_PASS')
 
 print(db_user)
 print(db_pass)
+
+# this is only a test file to test how the environmental variables work. 
+# so i can store some private information in my computer without showing it when i push my code
