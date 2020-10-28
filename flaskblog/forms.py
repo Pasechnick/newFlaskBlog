ef5f38944d4146dcb0159e18305ee22bcb634522
Alexander Mitupov <alex@Alexs-MBP.local>
2020-10-28 17:20:50 +0700

    add some comments for better understanding of code

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,16 +1,15 @@
 from flask_wtf import FlaskForm
-from flask_wtf.file import FileField, FileAllowed # need ths to upload files into the db, and allowance to be able to upload surtain files (validator)
-from flask_login import current_user # we need current user for update user info (for data check )
+from flask_wtf.file import FileField, FileAllowed # need ths to upload files into the db, and allowance to be able to upload certain files (validator)
+from flask_login import current_user # we need current user for update user info (for data check) so that the user do not change values of username and ect. if he doesn't make changes
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
-from flaskblog.models import User # we need to import User to make the validation Error message
+from flaskblog.models import User 
 
-# reg form inherits from FlaskForm
 
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', 
-                            validators=[DataRequired(), Length(min=2, max=20)]) # the length of the username must be from 2 to 20 characters
+                            validators=[DataRequired(), Length(min=2, max=20)]) 
     email = StringField('Email',
                             validators=[DataRequired(), Email()])
     password = PasswordField('Password', 
@@ -18,12 +17,6 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField('Confirm Password',
                             validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
-
-    # with this template function we can prevent showing the flaskerror page instead with our error messages under the sign up fields (username, email...)
-    # ^it is taken from "wtf_forms" documentaton 
-    # def validate_field(self, field):
-    #     if True:
-    #         raise ValidationError('Validation Message')
 
     def validate_username(self, username):
         user = User.query.filter_by(username = username.data).first()
@@ -42,16 +35,18 @@ class LoginForm(FlaskForm):
                             validators=[DataRequired(), Email()])
     password = PasswordField('Password', 
                             validators=[DataRequired()])
-    remember = BooleanField('Remember Me') # allows user to stay logged in for some time, using a secure cookie 
+    remember = BooleanField('Remember Me') 
     submit = SubmitField('Login')
 
 
+# also inherits from FlaskForm
 # the form for updating an account info in the account route
 class UpdateAccountForm(FlaskForm):
     username = StringField('Username', 
                             validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email',
                             validators=[DataRequired(), Email()])
+                        # 'Update Profile Picture' - label. with FileField we can accept images so we can update picture of account. 
     picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])]) # with FileAllowed validator we can validate particular file extention                            
     submit = SubmitField('Update')
 
@@ -59,7 +54,7 @@ class UpdateAccountForm(FlaskForm):
     # with current_user class we want to make a check, so if the username/email does not match to current user's we make an update 
     def validate_username(self, username):
         # the username validation check
-        if username.data != current_user.username:
+        if username.data != current_user.username: # so the data (username) we enter will be checked with the data from DB
             user = User.query.filter_by(username = username.data).first()
             if user:
                 raise ValidationError('Validation Message: That Username is taken, please use another')
--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -3,12 +3,10 @@ import secrets # we need in order to get randomize picture name when we upload a
 from PIL import Image # this class is installed with Pillow package (pip3 install Pillow) so we can autoresize big pictures to prevent the site from loading big files
 from flask import render_template, url_for, flash, redirect, request
 from flaskblog import app, db, bcrypt
-from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm # we need UpdateAccountForm so we can use
+from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm # we need UpdateAccountForm so we can use validation check
 from flaskblog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required # imports that class so we can check the user email and password validation with the db data, and with current user we can deliver user a
 
-
-# !! AFTER THE DB.MODELS ARE CREATED WE GO TO THE PYTHON3 CONSOLE AND ACTUALLY CREATE THE DATABASE WITH OUR MODELS !! 
 
 
 posts = [
@@ -36,57 +34,54 @@ def home():
 def about():
     return render_template("about.html")
 
-#  methods =['GET', 'POST'] list of allowed methods when we press sign up button 
 @app.route("/register", methods =['GET', 'POST'])
 def register():
-    if current_user.is_authenticated: # current user check
+    if current_user.is_authenticated:
         return redirect(url_for('home'))
     form = RegistrationForm()
     if form.validate_on_submit():
-        #so if the form valid to submit (all length and signs are correct) we can hash the password and do the authentication procedure 
-        hashed_pasword = bcrypt.generate_password_hash(form.password.data).decode('utf-8') # "form.password.data" is the password that user has entered and ".decode('utf-8')" is needed to decde from bytes to a regular string
-        user = User(username = form.username.data, email = form.email.data, password = hashed_pasword) # getting the data from user into the db, where we get allready hashed password with "password = hashed_pasword"
-        #saving the entered dta to the db
+        hashed_pasword = bcrypt.generate_password_hash(form.password.data).decode('utf-8') 
+        user = User(username = form.username.data, email = form.email.data, password = hashed_pasword) 
         db.session.add(user)
         db.session.commit()
-        # creating the message for successfull sign up with the flash message
-        flash(f'Your account has been created ! You are now able to log in', 'success') # with the help of format methode (f string) - f'text text {form.username.data}!' we can use placeholder inside of a text
-        return redirect(url_for('login')) # so after the form is sended we redirect the user at the home function -> @app.route("/home") def home(): return render_template("home.html", posts = posts) 
+        flash(f'Your account has been created ! You are now able to log in', 'success') 
+        return redirect(url_for('login'))
     return render_template('register.html', title = 'Register', form = form)
 
 
 @app.route("/login", methods =['GET', 'POST'])
 def login():
-    if current_user.is_authenticated: # current user check
+    if current_user.is_authenticated: 
         return redirect(url_for('home'))
     form = LoginForm()
-    #  creating some dummy data to check the login form 
+    
     if form.validate_on_submit():
-        # before we were simply checking the hardcoded data, and now we need to check the database for valid data 
-        # firstly the user will be logged in with an email, so we need to write a check for the email in the db:
-        user = User.query.filter_by(email = form.email.data).first() # finds the first User by entered email in the id
-        if user and bcrypt.check_password_hash(user.password, form.password.data): # so if the user exist and the password the user entered is valid we want to login the user and we also need to import the login user function 
-            login_user(user, remember = form.remember.data) # "user" - what we want to login, "remember" - remember me form (boolean) we have created
-            next_page = request.args.get('next') # this line 
-            return redirect(next_page) if next_page else redirect(url_for('home')) # use of ternary conditional 
+        user = User.query.filter_by(email = form.email.data).first() 
+        if user and bcrypt.check_password_hash(user.password, form.password.data): 
+            login_user(user, remember = form.remember.data)
+            next_page = request.args.get('next') 
+            return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
             flash("login unsuccessful. Pls check ur email  and password", "danger")
     return render_template('login.html', title = 'Login', form = form)
 
 
-# logout route we need ...
+
 @app.route("/logout")
 def logout():
     logout_user()
     return redirect(url_for('home'))
 
 
-# logic for uploading a new picture as avatar 
+# logic for uploading a new picture as avatar and saving in DB 
+# we take picture data as argument
 def save_picture(form_picture):
-    random_hex = secrets.token_hex(8) # saves picture we upload with random hex token 8 bytes
+    random_hex = secrets.token_hex(8) # saves picture we upload with random hex token 8 bytes. so there is no same names in the DB
+    # "os.path.splitext" function return two values: file name without the extention and the extention itself. So with underscore "_"  we through away the variable name and we grab the name (also can use "f_name"), we grab the extention with "f_ext"
+    # form_picture - will be the data from the field (name of the picture)
     _, f_ext = os.path.splitext(form_picture.filename) # we need the file extention at the end of the filename. we do not need to grab the file name so we can use underscore "_" to through away a variable name 
-    picture_fn = random_hex + f_ext
-    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn) #full path to the package directory
+    picture_fn = random_hex + f_ext # makes name for the uploaded picture ("picture_fn" - picture filename)
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn) # full path to the package directory
     
     # resizing picture (the upload image might be too big, so the page will load slowly in some cases), so we can change it to our needed size
     output_size = (125, 125) # sets the size...
@@ -103,24 +98,30 @@ def save_picture(form_picture):
 
 
 @app.route("/account", methods =['GET', 'POST'])
-@login_required #extention knows that we need to be logged in to access this route 
+@login_required 
 def account():
-    # creates an instance of that Update form 
+    # creates an instance of that update account form 
     form = UpdateAccountForm()
     if form.validate_on_submit():
-        # if the form is valid we can update data directly in SQL db
+        # looks for picture data 
         if form.picture.data: 
-            picture_file = save_picture(form.picture.data)
+            picture_file = save_picture(form.picture.data) # saves picture through "save_picture" function that accepts argument - the input data "form.picture.data" - the picture itself
+            # saves the uploaded picture as new picture for user
             current_user.image_file = picture_file
+        # if the form is valid we can update and commit data directly in SQL db
         current_user.username = form.username.data
         current_user.email = form.email.data
         db.session.commit()
         flash('Your account has been updated !', 'success')
-         # we redirect right after data submitted, cuz of get\post method
+         # we redirect right after data submitted, cuz of get\post method ("post-get redirect pattern")
         return redirect(url_for('account'))
+    # as soon as we getting redirected to account page it should populate the form with the current data of the user:
     elif request.method == "GET":
-        # populate the form with the current data of the user
         form.username.data = current_user.username
         form.email.data = current_user.email
-    image_file = url_for('static', filename='profile_pics/' + current_user.image_file) # it grabs the image in the user model (models.py) where we hardcoded that the default user image is the "default.jpg"
+    # here we set the image file that will be passed in the account template
+    # it grabs the "image_file" in the user model (models.py) where we hardcoded that the default user image is the "default.jpg"
+    image_file = url_for('static', filename='profile_pics/' + current_user.image_file) 
+    # we pass in the "image_file" variable into the account template (so it will be shown at "account.html") as additional argument. This way we can use now the "image_file" as source of the image
+    # we also pass in the instance of "updateAccountForm()" through "form" variable as argument, so we can use it in the account template
     return render_template('account.html', title = 'Account', image_file = image_file, form=form)
